c4bdeebd522476540ab2803ff31eb4b81828258a
Agustin Bustos Piasentini <bustos-agustin@hotmail.com - agustin.bustos.p@gmail.com>
2023-04-05 11:36:01 -0300

    Update: " - Procesado al Final para agregar compatibilidad con el consolidador de MC

--- Arreglo de archivos.py
+++ Arreglo de archivos.py
@@ -10,11 +10,20 @@ ruta = askdirectory()
 # Crear una lista con los nombres de los archivos en la carpeta
 archivos = os.listdir(ruta)
 
+# Filtrar los que no son '.xlsx'
+archivos = [archivo for archivo in archivos if '.xlsx' in archivo]
+
+# Quitar los archivos que empiezan con '~$'
+archivos = [archivo for archivo in archivos if '~$' not in archivo]
+
+# reemplazar '.xlsx' por ''
+archivos = [archivo.replace('.xlsx', '') for archivo in archivos]
+
 # Crear un dataframe con los nombres de los archivos
 Tabla_Archivos = pd.DataFrame(archivos, columns=['Archivo'])
 
 # Agregar la 'ruta' + '/' al 'Nompre'
-Tabla_Archivos['Archivo con ruta'] = ruta + '/' + Tabla_Archivos['Archivo']
+Tabla_Archivos['Archivo con ruta'] = ruta + '/' + Tabla_Archivos['Archivo'] + '.xlsx'
 
 # Crear una columna que se llame primera celda
 Tabla_Archivos['Primera celda'] = np.nan
@@ -39,20 +48,18 @@ for i in range(len(Tabla_Archivos)):
     # si la columna 'Archivo' contiene ' MCR ' entonces filtrar los datos donde el 'Tipo' contenga ' B'
     if ' MCR ' in Tabla_Archivos['Archivo'][i]:
         df = df[df['Tipo'].str.contains(' B') == False]
-        #Exportar el dataframe solo si contiene datos
-        if len(df) > 0:
-            df.to_excel(ruta + '/Procesado/' + "Procesado - " + Tabla_Archivos['Archivo'][i], index=False)
-    else:
-        if len(df) > 0:
-            df.to_excel(ruta + '/Procesado/' + "Procesado - " + Tabla_Archivos['Archivo'][i], index=False)
+    #Exportar el dataframe solo si contiene datos
+    if len(df) > 0:
+        df.to_excel(ruta + '/Procesado/' + Tabla_Archivos['Archivo'][i] + " - Procesado.xlsx", index=False)
+
 
     if len(df) > 0:
         #Al archivo recien creado, agregar la primera celda en la primera fila
-        wb = openpyxl.load_workbook(ruta + '/Procesado/' + "Procesado - " + Tabla_Archivos['Archivo'][i])
+        wb = openpyxl.load_workbook(ruta + '/Procesado/' + Tabla_Archivos['Archivo'][i] + " - Procesado.xlsx")
         ws = wb.active
         ws.insert_rows(1)
         ws['A1'] = Tabla_Archivos['Primera celda'][i]
-        wb.save(ruta + '/Procesado/' + "Procesado - " + Tabla_Archivos['Archivo'][i])
+        wb.save(ruta + '/Procesado/' + Tabla_Archivos['Archivo'][i] + " - Procesado.xlsx")
     
 del i, df, wb, ws
 
