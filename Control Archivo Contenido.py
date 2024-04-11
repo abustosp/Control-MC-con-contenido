035d5452866853303f65ddb3505d4e8972a9cf1b
Agustin Bustos Piasentini bustos-agustin@hotmail.com <63634574+abustosp@users.noreply.github.com>
2024-04-11 13:19:25 -0300

    Refactor Control Archivo Contenido.py to improve file handling and add message box

--- Control Archivo Contenido.py
+++ Control Archivo Contenido.py
@@ -2,43 +2,52 @@ import os
 import pandas as pd
 import numpy as np
 from tkinter.filedialog import askdirectory
+from tkinter.messagebox import showinfo
 
-# seleccionar una carpeta
-ruta = askdirectory()
+def control_contenido():
 
-# crear una lista con los nombres de los archivos en la carpeta
-archivos = os.listdir(ruta)
+    # seleccionar una carpeta
+    ruta = askdirectory()
 
-# Filtrar los archivos que tengan la extensión .xlsx
-archivos = [archivo for archivo in archivos if archivo.endswith('.xlsx')]
+    # crear una lista con los nombres de los archivos en la carpeta
+    archivos = os.listdir(ruta)
 
-# crear un dataframe con los nombres de los archivos
-df = pd.DataFrame(archivos, columns=['Archivo'])
+    # Filtrar los archivos que tengan la extensión .xlsx
+    archivos = [archivo for archivo in archivos if archivo.endswith('.xlsx')]
 
-# agregar la 'ruta' + '/' al 'Nompre'
-df['Archivo con ruta'] = ruta + '/' + df['Archivo']
+    # crear un dataframe con los nombres de los archivos
+    df = pd.DataFrame(archivos, columns=['Archivo'])
 
-# Crear una columna que se llame primera celda
-df['Primera celda'] = np.nan
+    # agregar la 'ruta' + '/' al 'Nompre'
+    df['Archivo con ruta'] = ruta + '/' + df['Archivo']
 
-# Por cada archivo en la columna 'Archivo' se debe leer el contnido de la primera celda y guardarlo en la columna 'Primera celda'
-# for i in range(len(df)):
-#     df['Primera celda'][i] = pd.read_excel(df['Archivo con ruta'][i], header=None).iloc[0,0]
-# del i
+    # Crear una columna que se llame primera celda
+    df['Primera celda'] = np.nan
 
-def obtener_primera_celda(df):
-    return pd.read_excel(df, header=None).iloc[0,0]
+    # Por cada archivo en la columna 'Archivo' se debe leer el contnido de la primera celda y guardarlo en la columna 'Primera celda'
+    # for i in range(len(df)):
+    #     df['Primera celda'][i] = pd.read_excel(df['Archivo con ruta'][i], header=None).iloc[0,0]
+    # del i
 
-df['Primera celda'] = df['Archivo con ruta'].apply(obtener_primera_celda)
+    def obtener_primera_celda(df):
+        return pd.read_excel(df, header=None).iloc[0,0]
 
-#Crear la columa 'CUIT Archivo' con el valor de archivo a partir de la posicion 20 con un largo de 11 caracteres
-df['CUIT Archivo'] = df['Archivo'].str.slice(19, 30)
+    df['Primera celda'] = df['Archivo con ruta'].apply(obtener_primera_celda)
 
-#Crear la columa 'CUIT Primera Celda' con el valor de los últimos 11 caracteres de la columna 'Primera celda'
-df['CUIT Primera Celda'] = df['Primera celda'].str.slice(-11)
+    #Crear la columa 'CUIT Archivo' con el valor de archivo a partir de la posicion 20 con un largo de 11 caracteres
+    df['CUIT Archivo'] = df['Archivo'].str.slice(19, 30)
 
-#Crear columna de 'Control' con el valor de 'SI' si los valores de las columnas 'CUIT Archivo' y 'CUIT Primera Celda' son iguales
-df['Control'] = np.where(df['CUIT Archivo'] == df['CUIT Primera Celda'], 'SI', 'NO')
+    #Crear la columa 'CUIT Primera Celda' con el valor de los últimos 11 caracteres de la columna 'Primera celda'
+    df['CUIT Primera Celda'] = df['Primera celda'].str.slice(-11)
 
-# Guardar el dataframe en un archivo excel
-df.to_excel('Resultado.xlsx', index=False)
+    #Crear columna de 'Control' con el valor de 'SI' si los valores de las columnas 'CUIT Archivo' y 'CUIT Primera Celda' son iguales
+    df['Control'] = np.where(df['CUIT Archivo'] == df['CUIT Primera Celda'], 'SI', 'NO')
+
+    # Guardar el dataframe en un archivo excel
+    df.to_excel('Resultado.xlsx', index=False)
+    
+    # Mostrar un mensaje de que el proceso ha finalizado
+    showinfo('Proceso finalizado', 'El proceso ha finalizado, revise el archivo Resultado.xlsx')
+    
+if __name__ == '__main__':
+    control_contenido()
